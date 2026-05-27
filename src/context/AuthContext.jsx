10d1267d295e14db52d7be68c20aab7a7e724503
Manik kori <manikkori697@gmail.com>
2026-05-27 23:02:33 +0530

    AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,22 +1,21 @@
-// AuthContext.jsx ke andar
+
 import { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    // STARTING ME HI LOCALSTORAGE SE TOKEN NIKALO
+   
     const [token, setToken] = useState(localStorage.getItem('token') || null);
     const [user, setUser] = useState(null);
 
-    // Jab bhi token change ho, user ki details backend se fetch karo
+  
     useEffect(() => {
         if (token) {
-            // Token ko har aage ki request ke liye default set kar do
+        
             axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
             
-            // Backend se user data manga lo (agar tumhara koi /api/auth/me route hai)
-            // Warna user data ko bhi localStorage me stringify karke rakh sakte ho.
+         
         } else {
             delete axios.defaults.headers.common['Authorization'];
         }
